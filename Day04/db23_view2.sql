88c16055436dc8da7b6d10fd3d614b57be03329f
Park-JuHyeon <j-hyeon97@naver.com>
2023-03-03 17:24:22 +0900

    3월 3일 금요일
    TGIF !!

---
+++ Day04/db23_view2.sql
@@ -0,0 +1,17 @@
+-- 뷰
+USE sqldb;
+
+CREATE VIEW uv_potentialUser
+AS 
+	SELECT u.*
+         , b.num, b.prodName, b.price, b.amount
+      FROM usertbl AS u
+      LEFT OUTER JOIN buytbl AS b
+        ON u.userID = b.userID
+	 WHERE b.userID IS NULL;
+     
+SELECT * FROM uv_potentialUser;
+
+-- 사용자 
+
+
